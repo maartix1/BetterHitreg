df37ea8fd032983667b1cd69c602e0ea9b4561c4
Jass <145933956+jasspir@users.noreply.github.com>
2026-06-10 17:50:33 -0400

    delete old commented code

--- src/main/java/you/jass/betterhitreg/mixin/ParticleMixin.java
+++ src/main/java/you/jass/betterhitreg/mixin/ParticleMixin.java
@@ -12,17 +12,6 @@ import you.jass.betterhitreg.settings.Toggle;
 
 import static you.jass.betterhitreg.hitreg.Hitreg.client;
 
-
-//@Mixin(WorldRenderer.class)
-//public class ParticleMixin {
-//    @Inject(method = "spawnParticle(Lnet/minecraft/particle/ParticleEffect;ZZDDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
-//    private void spawnParticle(ParticleEffect parameters, boolean alwaysSpawn, boolean canSpawnOnMinimal, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> cir) {
-//        if (Toggle.HIDE_ALL_PARTICLES.toggled()) cir.cancel();
-//        if (Toggle.HIDE_OTHER_PARTICLES.toggled() && parameters.getType() != ParticleTypes.CRIT && parameters.getType() != ParticleTypes.SWEEP_ATTACK) cir.cancel();
-//        if (Toggle.HIDE_OTHER_FIGHTS.toggled() && client.player != null && client.player.squaredDistanceTo(x, y, z) > 30) cir.cancel();
-//    }
-//}
-
 @Mixin(ParticleManager.class)
 public class ParticleMixin {
     @Inject(method = "createParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
